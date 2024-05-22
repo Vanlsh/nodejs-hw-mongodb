4ab677bef7ddb552ee645928e1fbd00cfa386530
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-05-22 22:42:59 +0300

    feat(contact): add create contact end-point

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,13 +1,19 @@
 import createHttpError from 'http-errors';
-import { getAllContacts, getContactById } from '../services/contacts.js';
+import {
+  getAllContacts,
+  getContactById,
+  createContact,
+} from '../services/contacts.js';
 
 export const getContactsController = async (_, res) => {
   const contacts = await getAllContacts();
+
   const resBody = {
     status: 200,
     data: contacts,
     message: 'Successfully found contacts!',
   };
+
   res.status(200).json(resBody);
 };
 
@@ -25,5 +31,15 @@ export const getContactByIdController = async (req, res, next) => {
     status: 200,
     data: contact,
     message: `Successfully found contact with id ${contactId}!`,
+  });
+};
+
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a contact!`,
+    data: contact,
   });
 };
