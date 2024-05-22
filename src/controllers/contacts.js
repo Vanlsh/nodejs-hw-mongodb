f115b5e38995fd912151eaa0f36b2d407cc59a63
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-05-22 22:59:32 +0300

    feat(contact): add patch contact end-point

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -3,6 +3,7 @@ import {
   getAllContacts,
   getContactById,
   createContact,
+  updateContact,
 } from '../services/contacts.js';
 
 export const getContactsController = async (_, res) => {
@@ -39,7 +40,24 @@ export const createContactController = async (req, res) => {
 
   res.status(201).json({
     status: 201,
-    message: `Successfully created a contact!`,
+    message: 'Successfully created a contact!',
     data: contact,
+  });
+};
+
+export const patchContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result.contact,
   });
 };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,6 +3,7 @@ import {
   getContactsController,
   getContactByIdController,
   createContactController,
+  patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 const router = Router();
@@ -12,5 +13,7 @@ router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 
 router.post('/contacts', ctrlWrapper(createContactController));
+
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 
 export default router;
